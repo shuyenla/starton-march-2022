f328289f1f7c9668b7cf5731b1216a0275e0ec25
lpellier <pellierlucas@gmail.com>
2022-03-20 16:14:50 +0100

    removed backend, we dont need that

--- backend/dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="multer" />
-import { AppService } from './app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    uploadMusic(music: Express.Multer.File): Promise<any>;
-}
